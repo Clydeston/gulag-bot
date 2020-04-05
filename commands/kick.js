b902b3c3642e426960a93ec5baf0089677b62495
Clydeston <Clydeston@users.noreply.github.com>
2020-04-05 14:12:10 +0100

    fixed securiy issues

--- commands/kick.js
+++ commands/kick.js
@@ -3,16 +3,28 @@ exports.run = (bot, message, args) => {
     let guild = bot.guilds.cache.get("694513340931768340");
     const user_to_kick = message.mentions.users.first();
 
-    if(user_to_kick) {
-        let user = guild.member(user_to_kick);
-        if(!args[1]) {
-            global.data.sendMessageToChannel(bot, message.channel.id, "Please provide a valid reason for the kick!");
+    var access_command = false;
+    var required_roles = global.data.getCommandPermissions("kick");
+    required_roles.forEach(element => {
+      if(message.member.roles.cache.some(role => role.name === element.name)) {
+        access_command = true;
+      }
+    });
+  
+    if(access_command) {        
+        if(user_to_kick) {
+            let user = guild.member(user_to_kick);
+            if(!args[1]) {
+                global.data.sendMessageToChannel(bot, message.channel.id, "Please provide a valid reason for the kick!");
+            }else {
+                kick(user, message, args[1], bot);
+            }
         }else {
-            kick(user, message, args[1], bot);
+            global.data.sendMessageToChannel(bot, message.channel.id, "Please provide a valid mention!");
         }
-    }else {
-        global.data.sendMessageToChannel(bot, message.channel.id, "Please provide a valid mention!");
-    }
+    } else {
+        message.reply(" Sorry you require elevated permissions!");
+    } 
 }
 
 function kick(user, message, reason_for_kick, bot) {
