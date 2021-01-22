9315217f7ba97ad5d2255673629e1c2e6f075a33
Clydeston <Clydeston@users.noreply.github.com>
2021-01-22 09:19:52 +0000

    added retard.js

---
+++ commands/retard.js
@@ -0,0 +1,32 @@
+const global = require("../global.js");
+
+exports.run = (bot, message, args) => {    
+    const user_to_give_role = message.mentions.users.first();
+    let guild = bot.guilds.cache.get("694513340931768340");
+
+    var access_command = false;
+    var required_roles = global.data.getCommandPermissions("kick");
+    required_roles.forEach(element => {
+        if(message.member.roles.cache.some(role => role.name === element.name)) {
+        access_command = true;
+        }
+    });
+
+    if(access_command) {        
+        if(!user_to_give_role) {
+            global.data.sendMessageToChannel(bot, message.channel.id, "Please provide a valid mention!");
+        }else{
+            let user = guild.member(user_to_give_role);
+            var role = guild.roles.cache.get("801941666306588675");
+            if(user.roles.cache.find(r => r.id == "801941666306588675")){
+                global.data.sendMessageToChannel(bot, message.channel.id, "User is already dribbling!");
+            }else{
+                user.roles.add(role);
+                global.data.sendMessageToChannel(bot, message.channel.id, "Member is now retarded!");
+            }
+        }
+    } else {
+        message.reply("Sorry you require elevated permissions!");
+    }   
+}
+// wrote this while tired - TODO CHECK LATER
